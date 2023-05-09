e8c9e7ac0d4d1e3afe7b962d477b917bbbf8a19f
YexinioAguero <yexinio.aguero@ucr.ac.cr>
2023-05-09 17:42:21 -0600

    Controladores user y super adim

--- Videoteca/Controllers/SuperAdminController.cs
+++ Videoteca/Controllers/SuperAdminController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace Videoteca.Controllers
 {
+    [Authorize(Roles = "super_admin")]
     public class SuperAdminController : Controller
     {
         public IActionResult Index()
