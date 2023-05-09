0f5ad3232e4e7c22976a3a7506ef25a430ee59e3
YexinioAguero <yexinio.aguero@ucr.ac.cr>
2023-05-09 15:55:43 -0600

    Agregar archivos de proyecto.

---
+++ Videoteca/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Videoteca.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
