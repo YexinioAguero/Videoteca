12d539fbd7fbaf7a1b2c363540b47da86a7e8dd9
AlexUreñaJiménez <ALEX.URENA@ucr.ac.cr>
2023-05-26 10:51:14 -0600

    Se agregó el modificar usuarios desde el super admin

--- Videoteca/Controllers/UsersController.cs
+++ Videoteca/Controllers/UsersController.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using Videoteca.Data;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json.Linq;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Model;
+using Videoteca.Migrations;
 
 namespace Videoteca.Controllers
 {
@@ -12,24 +16,22 @@ namespace Videoteca.Controllers
         // GET: PersonController
         public ActionResult Index()
         {
-            var personList = new List<User>();
+            var personList = new List<AspNetUser>();
             using (var dbContext = new VideotecaContext())
             {
-
-                personList = dbContext.Users.ToList();
-
+                personList = dbContext.AspNetUsers.ToList();
             }
             ViewBag.persona = personList.FirstOrDefault();
             return View(personList);
         }
 
         // GET: PersonController/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(string id)
         {
-            var person = db.Users.Find(id);
-            db.Users.Where(c => c.user_id == id);
+            var person = db.AspNetUsers.Find(id);
+            db.AspNetUsers.Where(c => c.Id == id);
 
-            return View(User);
+            return View(person);
         }
 
         // GET: PersonController/Create
@@ -53,26 +55,41 @@ namespace Videoteca.Controllers
             }
         }
 
+
+
+
         // GET: PersonController/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(string id)
         {
-            return View();
+            var person = db.AspNetUsers.Find(id);
+
+            return View(person);
         }
 
         // POST: PersonController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(string id, AspNetUser person)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+     
+                db.Update(person);
+                db.SaveChanges(true);
+                ViewBag.Message = new MessagePack { Text = "Se realizo de manera correcta", Tipo = Tipo.message.success.ToString() };
+                return View();
             }
             catch
             {
+                ViewBag.Message = new MessagePack { Text = "No se realizo de manera correcta", Tipo = Tipo.message.danger.ToString() };
+
                 return View();
             }
         }
+
+
+
+
 
         // GET: PersonController/Delete/5
         public ActionResult Delete(int id)
@@ -96,3 +113,33 @@ namespace Videoteca.Controllers
         }
     }
 }
+
+//< div class= "row" >
+//    < div class= "col-md-4" >
+//        < form asp - action = "Edit" >
+//            < div asp - validation - summary = "ModelOnly" class= "text-danger" ></ div >
+//            < input type = "hidden" asp -for= "Id" />
+//            < div class= "form-group" >
+//                < label asp -for= "Name" class= "control-label" ></ label >
+//                < input asp -for= "Name" class= "form-control" />
+//                < span asp - validation -for= "Name" class= "text-danger" ></ span >
+//            </ div >
+
+//            < div class= "form-group" >
+//                < label asp -for= "UserName" class= "control-label" ></ label >
+//                < input asp -for= "UserName" class= "form-control" />
+//                < span asp - validation -for= "UserName" class= "text-danger" ></ span >
+//            </ div >
+
+//            < div class= "form-group" >
+//                < label asp -for= "Email" class= "control-label" ></ label >
+//                < input asp -for= "Email" class= "form-control" />
+//                < span asp - validation -for= "Email" class= "text-danger" ></ span >
+//            </ div >
+
+//            < div class= "form-group" >
+//                < input type = "submit" value = "Save" class= "btn btn-primary" />
+//            </ div >
+//        </ form >
+//    </ div >
+//</ div >
---
+++ Videoteca/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace Videoteca
+{
+  
+        public class MessagePack
+        {
+            public string Text { get; set; }
+
+            public string Tipo { get; set; }
+        }
+    
+}
